3e4045627c8f58c6514444b56261b832c3e19a6c
Ulisse Tucci <ulissetucci@gmail.com>
2022-10-28 17:12:35 +0200

    Aggiunta delle funzioni per il testing

--- src/test/java/it/unical/FunnyAlgorithmsTest.java
+++ src/test/java/it/unical/FunnyAlgorithmsTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,7 +21,7 @@ public class FunnyAlgorithmsTest {
 	@BeforeClass
 	public static void beforClass() {
 		funny = new FunnyAlgorithms();
-		System.out.println("before class!");
+		System.out.println("before class!"); 
 	}
 	
 	@AfterClass
@@ -38,11 +39,39 @@ public class FunnyAlgorithmsTest {
 		System.out.println("after!");
 	}
 	
+	@Test
+	public void binarySearchShouldFail(){
+	    DateTime start = new DateTime();
+	    System.out.println(start);
+	    
+	    int[] numBinarySearch = new int[]{10, 20, 30, 40, 50, 60, 70, 80};
+	    Integer result = funny.binarySearch(numBinarySearch, 75);
+	    assertTrue(result.equals(-1));
+	    
+	    DateTime fine = new DateTime();
+	    System.out.println(fine);
+	}
+	
+	
+	@Test(timeout=500)
+	public void BinarySearchIsFastEnough(){
+	    DateTime start = new DateTime();
+	    System.out.println(start);
+	    
+	    int arrBinarySearch [] = {1, 2, 3, 4, 5, 6, 7, 8};
+	    funny.binarySearch(arrBinarySearch, 8);
+	    System.out.println("Velocità buona");
+	    
+	    DateTime fine = new DateTime();
+	    System.out.println(fine);
+	}
+	
+	
     @Test
     public void stringToIntConverterTest() {
     	// Caso di prova n°1:
     	String str1 = "10";
-    	assertEquals(11, funny.stringToIntConverter(str1));
+    	assertEquals(10, funny.stringToIntConverter(str1));
     	// Caso di prova n°2:
     	String str2 = "15";
     	assertEquals(15, funny.stringToIntConverter(str2));
